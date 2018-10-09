29ad7064f3b25495bab3245b39b6351c661d67d9
Hakanc16 <Hakan-Cokkisku@live.de>
2018-10-09 11:54:07 +0200

    INIT

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        blatt1Aufgabe1();
+    }
+
+    public static void blatt1Aufgabe1() {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Geben Sie eine Zahl ein");
+            int Zahl = in.nextInt();
+            int Ergebnis = Zahl % 17;
+            System.out.println("Der Rest bei Division durch " + 17 + " ist " + Ergebnis);
+
+        }
+
+    public static void blatt1Aufgabe2() {
+        //kann nicht gelöst werden, weil wir die Dateien nicht dazu haben.
     }
 }
