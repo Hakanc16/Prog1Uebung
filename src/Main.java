d174e411af35b836760f01fd60b422f8e37f5e12
Hakanc16 <Hakan-Cokkisku@live.de>
2018-10-09 14:27:32 +0200

    INIT

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        blatt2Aufgabe2();
+        blatt2Aufgabe3();
     }
 
     public static void blatt1Aufgabe1() {
@@ -20,17 +19,17 @@ public class Main {
         //kann nicht gelöst werden, weil wir die Dateien nicht dazu haben.
     }
 
-    public static void blatt2Aufgabe1 () {
+    public static void blatt2Aufgabe1() {
         boolean x;
         boolean y;
 
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         System.out.println("x");
-        x = sc.nextBoolean();
+        x = in.nextBoolean();
 
         System.out.println("y");
-        y = sc.nextBoolean();
+        y = in.nextBoolean();
 
         if (x && y) {
             System.out.println("false");
@@ -42,7 +41,7 @@ public class Main {
     public static void blatt2Aufgabe2() {
 
         Scanner in = new Scanner(System.in);
-        int x,y;
+        int x, y;
 
         System.out.println("Geben Sie eine Zahl für X ein:");
         x = in.nextInt();
@@ -50,14 +49,41 @@ public class Main {
         System.out.println("Geben Sie ein Zahl für Y ein:");
         y = in.nextInt();
 
-        if((x > 0) || (y > 0)) {
-        } if(x > y) {
+        if ((x > 0) || (y > 0)) {
+        }
+        if (x > y) {
             System.out.println(x);
         } else {
             System.out.println(y);
         }
         if ((x & y) < 0) {
             System.out.println(0);
+        }
+    }
+
+    public static void blatt2Aufgabe3() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Geben Sie eine Zahl für n ein:");
+        int n = in.nextInt();
+
+        System.out.println("Hier sind deine ersten " + n + " ungeraden Zahlen:");
+        for (int i = 1; i <= n * 2; i = i + 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Hier sind deine ersten " + n + " geraden Quadratzahlen:");
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i * i);
+        }
+
+        System.out.println("Hier sind deine ersten " + n + " positiven Zahlen, die durch 13 teilbar sind:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i * 13);
+        }
+
+        System.out.println("Hier sind deine ersten " + n + " positiven geraden Zahlen rückwärts:");
+        for (int i = n; i > 0; i--) {
+            System.out.println(i*2);
         }
     }
 }
